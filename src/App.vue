e3147d35ef78fc9d48358b6b027b0d0c2d622aca
Serdar Ergün <generalgord@gmail.com>
2022-06-03 17:13:15 +0300

    Sample nav

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
   <h1>Hello, I'm V.</h1>
+  <MainNav />
 </template>
 
 <script>
+import MainNav from "@/components/MainNav.vue";
+
 export default {
   name: "App",
+  components: {
+    MainNav,
+  },
 };
 </script>
 
---
+++ src/components/MainNav.vue
@@ -0,0 +1,8 @@
+<template lang="tr">
+  <header class="w-full text-sm">Main Nav</header>
+</template>
+<script>
+export default {
+  name: "MainNav",
+};
+</script>
