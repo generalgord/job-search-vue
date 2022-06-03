896b7c4323d00406703223d53f6d4a1be1a341e6
Serdar Ergün <generalgord@gmail.com>
2022-06-03 16:20:05 +0300

    Tailwind font added

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,12 +5,13 @@ module.exports = {
   },
   extends: [
     "plugin:vue/vue3-recommended",
-    "eslint:recommended",
     "plugin:prettier/recommended",
+    "eslint:recommended",
   ],
   parserOptions: {
     parser: "@babel/eslint-parser",
   },
+
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
