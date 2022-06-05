697e4e4a1d20c018f805d2558f200f284d331fab
Serdar Ergün <generalgord@gmail.com>
2022-06-05 22:52:06 +0300

    Introduction to Vue Computed with a simple example

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -1,15 +1,48 @@
 <template>
-  <button
-    class="px-5 py-3 font-medium text-white border-0 rounded bg-brand-blue-1 hover:shadow-blue"
-  >
-    Sign in
+  <button :class="{ primary: primary }">
+    {{ text }}
+  </button>
+  <button :class="{ primary: primary }" @click="doubleHeight">
+    {{ area }}
   </button>
 </template>
 
 <script>
 export default {
   name: "ActionButton",
+  props: {
+    text: {
+      type: String,
+      required: true,
+      default: "Action",
+    },
+  },
+  data() {
+    return {
+      primary: true,
+      width: 10,
+      height: 5,
+    };
+  },
+  computed: {
+    area() {
+      return this.width * this.height;
+    },
+  },
+  methods: {
+    doubleHeight() {
+      this.height = this.height * 2;
+    },
+  },
 };
 </script>
 
-<style></style>
+<style scoped>
+button {
+  @apply px-5 py-3 font-medium rounded;
+}
+
+.primary {
+  @apply text-white border-0 bg-brand-blue-1 hover:shadow-blue;
+}
+</style>
--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,12 +1,8 @@
 <template>
-  <header class="w-full text-sm">
-    <div class="fixed top-0 left-0 h-16 w-full bg-white">
-      <div
-        class="flex flex-nowrap h-full px-8 mx-auto border-b border-solid border-brand-gray-1"
-      >
-        <a :href="url" class="flex items-center h-full text-xl">{{
-          company
-        }}</a>
+  <header class="w-full py-2 text-sm border-b border-solid border-brand-gray-1">
+    <div class="w-full bg-white">
+      <div class="flex flex-nowrap items-center h-full px-8 mx-auto">
+        <a :href="url" class="flex h-full text-xl">{{ company }}</a>
 
         <nav class="h-full ml-12">
           <ul class="flex h-full p-0 m-0 list-none space-x-9">
@@ -16,16 +12,19 @@
               class="h-full"
               data-test="main-nav-list-item"
             >
-              <a href="" class="flex items-center h-full py-2.5">{{
-                menuItem
-              }}</a>
+              <a href="" class="flex h-full py-2.5">{{ menuItem }}</a>
             </li>
           </ul>
         </nav>
 
-        <div class="flex items-center h-full ml-auto">
+        <div class="flex h-full ml-auto space-x-5">
           <profile-image v-if="isLoggedIn" data-test="profile-image" />
-          <action-button v-else data-test="login-button" @click="loginUser" />
+          <action-button
+            v-else
+            text="Sign In"
+            data-test="login-button"
+            @click="loginUser"
+          />
         </div>
       </div>
     </div>
