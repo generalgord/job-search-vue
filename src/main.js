c37b37cf9221c8397780e86beb3e15470d43039b
Serdar Ergün <generalgord@gmail.com>
2022-06-16 13:38:09 +0300

    Some root fixing

--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
-import App from "./App.vue";
-import "./assets/tailwind.css";
-import router from "./router";
+import App from "@/App.vue";
+import "@/assets/tailwind.css";
+import router from "@/router";
 
 library.add(faSearch);
 
