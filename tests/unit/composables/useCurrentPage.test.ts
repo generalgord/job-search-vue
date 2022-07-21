6506d06750216a8fbe3987b955212b29b43580b5
Serdar Ergün <generalgord@gmail.com>
2022-07-21 19:53:50 +0300

    Refactored composables to TS

--- tests/unit/composables/useCurrentPage.test.ts
+++ tests/unit/composables/useCurrentPage.test.ts
@@ -3,17 +3,19 @@ jest.mock("vue-router");
 
 import useCurrentPage from "@/composables/useCurrentPage";
 
+const useRouteMock = useRoute as jest.Mock;
+
 describe("useCurrentPage", () => {
   describe("when query params include page", () => {
     it("returns page", () => {
-      useRoute.mockReturnValue({ query: { page: "3" } });
+      useRouteMock.mockReturnValue({ query: { page: "3" } });
       const result = useCurrentPage();
       expect(result.value).toBe(3);
     });
   });
   describe("when query params exlude page", () => {
     it("defaults to page 1", () => {
-      useRoute.mockReturnValue({ query: {} });
+      useRouteMock.mockReturnValue({ query: {} });
       const result = useCurrentPage();
       expect(result.value).toBe(1);
     });
